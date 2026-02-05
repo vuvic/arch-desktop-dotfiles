deb8962a680129e0d7d22ff341c278ce15703842
vtmdv <vuvictor@proton.mail>
2026-02-05 13:44:24 -0800

    Added AUR package installation to install-packages.sh

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -26,6 +26,29 @@ function enable_pacman_repo ()
   fi
 }
 
+# Installs yay - a pacman helper for AUR packages
+install_yay() {
+  # "If the command "yay" works"
+  # Also send the output - either file descriptor 1 (stdout) to /dev/null (discards anything written to it)
+  # or file descriptor 2 (sterr) where file descriptor 1 would otherwise go; &indicates that what follows 
+  # is a file descriptor, not a filename
+  if command -v yay >/dev/null 2>&1; then
+    echo "yay already installed"
+    return 0
+  fi
+
+  echo "Installing yay..."
+
+  sudo pacman -S --needed --noconfirm base-devel git
+
+  tmpdir=$(mktemp -d)
+  git clone https://aur.archlinux.org/yay.git "$tmpdir/yay"
+  (cd "$tmpdir/yay" && makepkg -si --noconfirm)
+
+  rm -rf "$tmpdir"
+}
+
+
 # Enable multilib and extra repositories
 for repo in multilib extra; do
   enable_pacman_repo "$repo"
@@ -33,8 +56,14 @@ done
 
 
 # Install packages from list
-input="$HOME/dotfiles/packages/pkg-list.txt" 
-echo "Fetching files from $input..."
-sudo pacman -S --needed - < "$input"
+pkg_list="$HOME/dotfiles/packages/pkg-list.txt" 
+echo "Fetching files from $pkg_list..."
+sudo pacman -S --needed - < "$pkg_list"
 
-
+# Install AUR packages if pkg-list-aur.txt exists and has at least 1 item
+aur_list="$HOME/dotfiles/packages/pkg-list-aur.txt"
+if [[ -f "$aur_list" && -s "$aur_list" ]]; then
+  install_yay
+  echo "Installing AUR packages from $aur_list..."
+  yay -S --needed - < "$aur_list"
+fi
